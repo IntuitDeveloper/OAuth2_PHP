553942caad4bca20a87ee6d0a70455bff50f3b89
hlu2 <Hao_Lu@intuit.com>
2017-07-17 10:08:49 -0700

    Add License

--- OAuth_2/config.php
+++ OAuth_2/config.php
@@ -1,14 +1,14 @@
 <?php
 return array(
-  'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
-  'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-  'client_id' => 'Q0wDe6WVZMzyu1SnNPAdaAgeOAWNidnVRHWYEUyvXVbmZDRUfQ',
-  'client_secret' => 'R9IttrvneexLcUZbj3bqpmtsu5uD9p7UxNMorpGd',
-  'oauth_scope' => 'com.intuit.quickbooks.accounting',
-  'openID_scope' => 'openid profile email',
-  'oauth_redirect_uri' => 'https://d1eec721.ngrok.io/OAuth_2/OAuth2PHPExample.php',
-  'openID_redirect_uri' => 'https://d1eec721.ngrok.io/OAuth_2/OAuthOpenIDExample.php',
-  'mainPage' => 'https://d1eec721.ngrok.io/OAuth_2/index.php',
-  'refreshTokenPage' => 'https://d1eec721.ngrok.io/OAuth_2/RefreshToken.php'
+  'authorizationRequestUrl' => '', //Example https://appcenter.intuit.com/connect/oauth2',
+  'tokenEndPointUrl' => '', //Example https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+  'client_id' => '', //Example 'Q0wDe6WVZMzyu1SnNPAdaAgeOAWNidnVRHWYEUyvXVbmZDRUfQ',
+  'client_secret' => '', //Example 'R9IttrvneexLcUZbj3bqpmtsu5uD9p7UxNMorpGd',
+  'oauth_scope' => '', //Example 'com.intuit.quickbooks.accounting',
+  'openID_scope' => '', //Example 'openid profile email',
+  'oauth_redirect_uri' => '', //Example https://d1eec721.ngrok.io/OAuth_2/OAuth2PHPExample.php',
+  'openID_redirect_uri' => '',//Example 'https://d1eec721.ngrok.io/OAuth_2/OAuthOpenIDExample.php',
+  'mainPage' => '', //Example https://d1eec721.ngrok.io/OAuth_2/index.php',
+  'refreshTokenPage' => '', //Example https://d1eec721.ngrok.io/OAuth_2/RefreshToken.php'
 )
  ?>
